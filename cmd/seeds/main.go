64f3eed4ce872415c241fd195ba45626801b3dd0
heyzec <61238538+heyzec@users.noreply.github.com>
2023-10-14 13:45:35 +0800

    feat: refactor and improve seeding

--- cmd/seeds/main.go
+++ cmd/seeds/main.go
@@ -9,6 +9,5 @@ import (
 func main() {
 	dsn := "host=localhost user=postgres password=postgres dbname=govtech_assignment port=5432 sslmode=disable TimeZone=Asia/Singapore"
 	db, _ := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	seeds.CreateStudents(db)
-	seeds.CreateTeachers(db)
+	seeds.CreatePeople(db)
 }
